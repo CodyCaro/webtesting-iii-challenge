674f2e485264c579d9419990e0d6acd603843153
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-10-06 16:45:05 -0700

    Finished first test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
-// Test away
+// Test away!
+import React from "react";
+import { render, shallow } from "react-testing-library";
+import Dashboard from "./Dashboard";
+
+test("Gate defaults to Unlocked and Open", () => {
+  const wrapper = render(<Dashboard />);
+  expect(wrapper.baseElement).toMatchSnapshot();
+});
