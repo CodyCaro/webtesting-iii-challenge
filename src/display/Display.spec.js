b6ec37d32fc3daa7523e669fe8c754186034b131
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-10-06 18:17:36 -0700

    checks if open/closed and locked/unlocked displays

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,24 @@
 // Test away!
+// Test away!
+// Test away!
+import React from "react";
+import { render, shallow } from "react-testing-library";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "./Display";
+
+// test("Gate defaults to Unlocked and Open", () => {
+//   const wrapper = render(<Controls />);
+//   expect(wrapper.baseElement).toMatchSnapshot();
+// });
+
+test("Display if gate is open/closed and if locked/unlocked", () => {
+  const wrapper = render(<Display />);
+  //   const openCloseButton = wrapper.queryByText();
+  //   console.log(wrapper);
+  const openCloseDisplay = wrapper.queryByTestId("open-close-display");
+  const lockedUnlockedDisplay = wrapper.queryByTestId(
+    "locked-unlocked-display"
+  );
+  expect(openCloseDisplay).toBeTruthy();
+  expect(lockedUnlockedDisplay).toBeTruthy();
+});
