89a12d85fd023b12f3c2e05447553cec752bfb53
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-10-06 17:46:35 -0700

    Got it to check if the open closed button is disabled upon being locked

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,19 @@
 // Test away!
+// Test away!
+import React from "react";
+import { render, shallow } from "react-testing-library";
+import "@testing-library/jest-dom/extend-expect";
+import Controls from "./Controls";
+
+// test("Gate defaults to Unlocked and Open", () => {
+//   const wrapper = render(<Controls />);
+//   expect(wrapper.baseElement).toMatchSnapshot();
+// });
+
+test("Gate cannot be closed or opened if locked", () => {
+  const wrapper = render(<Controls locked={true} />);
+  //   const openCloseButton = wrapper.queryByText();
+  //   console.log(wrapper);
+  const openCloseButton = wrapper.queryByTestId("open-close");
+  expect(openCloseButton).toHaveAttribute("disabled");
+});
